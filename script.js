9e421eb9fd6d7a3aea58e71d50e55803a3ad6a02
LuckyMcDev <lucky.dev@myyahoo.com>
2025-07-29 15:44:50 +0200

    update

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const elements = document.querySelectorAll(".fade-in");
-    elements.forEach((el, index) => {
-        setTimeout(() => {
-            el.style.opacity = "1";
-        }, index * 500);
-    });
-});
